495dbad63ed2dfae74138b1861b01c3f3b2b3752
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-02-18 10:02:44 +0100

    Javadoc

--- intlist/src/intlist/IntList.java
+++ intlist/src/intlist/IntList.java
@@ -1,5 +1,7 @@
 package intlist;
 
+import java.util.Arrays;
+
 class Node {
 	int element;
 	Node next;
@@ -17,11 +19,29 @@ public class IntList {
 		throw new RuntimeException("Not yet implemented");
 	}
 	
+	public int[] getElements() {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+	/**
+	 * Initializes this IntList object with an empty list of elements.
+	 * 
+	 * @post This object's size is zero.
+	 *     | getSize() == 0
+	 */
 	public IntList() {
 		throw new RuntimeException("Not yet implemented");
 	}
 	
-	public void add(int element) {
+	/**
+	 * Adds the given element to the front of the list of integers stored by this object.
+	 * 
+	 * @post This object's list of integers equals the old list of integers with the given element added to the front
+	 *     | getElements().length == old(getElements()).length + 1 &&
+	 *     | getElements()[0] == element &&
+	 *     | Arrays.equals(getElements(), 1, getElements().length, old(getElements()), 0, old(getElements()).length)
+	 */
+	public void addFirst(int element) {
 		throw new RuntimeException("Not yet implemented");
 	}
 
