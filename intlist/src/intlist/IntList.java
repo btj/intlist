7d38deac2397a0310e9512eddaec47993d66bc45
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-02-24 12:38:29 +0100

    Array-based implementation

--- intlist/src/intlist/IntList.java
+++ intlist/src/intlist/IntList.java
@@ -2,35 +2,20 @@ package intlist;
 
 import java.util.Arrays;
 
-class Node {
-	int element;
-	Node next;
-}
-
 public class IntList {
 	
-	private Node firstNode;
+	private int[] elements;
 	
 	public int getSize() {
-		int count = 0;
-		for (Node n = firstNode; n != null; n = n.next)
-			count++;
-		return count;
+		return this.elements.length;
 	}
 	
 	public int get(int index) {
-		Node n = firstNode;
-		for (; index != 0; n = n.next, index--);
-		return n.element;
+		return this.elements[index];
 	}
 	
 	public int[] getElements() {
-		int[] elements = new int[getSize()];
-		Node n = firstNode;
-		int i = 0;
-		for (; n != null; n = n.next, i++)
-			elements[i] = n.element;
-		return elements;
+		return Arrays.copyOf(this.elements, this.elements.length);
 	}
 	
 	/**
@@ -40,43 +25,43 @@ public class IntList {
 	 *     | getSize() == 0
 	 */
 	public IntList() {
+		this.elements = new int[0];
 	}
 	
 	/**
 	 * Adds the given element to the front of the list of integers stored by this object.
 	 * 
 	 * @post This object's list of integers equals the old list of integers with the given element added to the front
-	 *     | getElements().length == old(getElements()).length + 1 &&
+	 *     | getSize() == old(getSize()) + 1 &&
 	 *     | getElements()[0] == element &&
-	 *     | Arrays.equals(getElements(), 1, getElements().length, old(getElements()), 0, old(getElements()).length)
+	 *     | Arrays.equals(getElements(), 1, getSize(), old(getElements()), 0, old(getSize()))
 	 */
 	public void addFirst(int element) {
-		Node n = new Node();
-		n.element = element;
-		n.next = firstNode;
-		firstNode = n;
+		int[] newElements = new int[this.elements.length + 1];
+		newElements[0] = element;
+		for (int i = 0; i < elements.length; i++)
+			newElements[i + 1] = elements[i];
+		elements = newElements;
 	}
 
 	/**
 	 * Adds the given element to the back of the list of integers stored by this object.
 	 * 
 	 * @post This object's list of integers equals the old list of integers with the given element added to the back
-	 *     | getElements().length == old(getElements()).length + 1 &&
-	 *     | getElements()[getElements().length - 1] == element &&
-	 *     | Arrays.equals(getElements(), 0, getElements().length - 1, old(getElements()), 0, old(getElements()).length)
+	 *     | getElements().length == old(getSize()) + 1 &&
+	 *     | getElements()[getSize() - 1] == element &&
+	 *     | Arrays.equals(getElements(), 0, getSize() - 1, old(getElements()), 0, old(getSize()))
 	 */
 	public void add(int element) {
-		if (firstNode == null) {
-			firstNode = new Node();
-			firstNode.element = element;
-		} else {
-			Node lastNode = firstNode;
-			while (lastNode.next != null) {
-				lastNode = lastNode.next;
-			}
-			lastNode.next = new Node();
-			lastNode.next.element = element;
-		}
+		int[] newElements = new int[this.elements.length + 1];
+		newElements[this.elements.length] = element;
+		for (int i = 0; i < elements.length; i++)
+			newElements[i] = elements[i];
+		elements = newElements;
+	}
+	
+	public void set(int index, int value) {
+		this.elements[index] = value;
 	}
 
 }
